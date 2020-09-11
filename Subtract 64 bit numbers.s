4c6e6baa35c7479753e66e5254553cce127541e7
PrashCode5321 <67261526+PrashCode5321@users.noreply.github.com>
2020-09-11 09:12:53 +0530

    Add files via upload

---
+++ Subtract 64 bit numbers.s
@@ -0,0 +1,23 @@
+		AREA main, CODE, READONLY 
+        EXPORT __main 
+__main  PROC
+        
+		
+		LDR R0, =0X12345678		;Number 1 -- R1_R0
+		LDR R1, =0X13141517
+		
+		LDR R2, =0X22FA23B1		;Number 2 -- R3_R2
+		LDR R3, =0X11223344
+		
+		;Subtraction of Number 2 and Number 1
+		
+		SUBS R5, R2, R0			;Substract lower 32 bits
+		SBCS R4, R3, R1			;Subtract higher 32 bits	
+		
+
+						 
+__stop  B __stop
+
+ 
+
+        ENDP
